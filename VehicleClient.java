916099e03f561fb126b9df0463bb4592ee8b93c0
seabastion <v8sebas@gmail.com>
2014-09-17 20:26:22 -0500

    Update VehicleClient.java

--- VehicleClient.java
+++ VehicleClient.java
@@ -1,4 +1,12 @@
+package folder1;
+import folder1.folder2;
+import java.utils.Scanner;
 /*
  * Author: Sebastian Hall
- * 
+ * Date: 2014-9-17
  */
+public class VehicleClient{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(S)
+  }
+}
